03f086e5721a307f8139f410b183fec09deb2168
innneang <saratlim@gmail.com>
2019-06-02 16:11:10 +0700

    Tweet this box added

--- themes/hugo-sustain/static/js/main.js
+++ themes/hugo-sustain/static/js/main.js
@@ -2,13 +2,16 @@
  * Created by fabiomadeira on 25/02/15.
  */
 // jQuery for page scrolling feature
-jQuery(document).ready(function(e) {
-    e(".scroll").click(function(t) {
+jQuery(document).ready(function (e) {
+    e(".scroll").click(function (t) {
         t.preventDefault();
         e("html,body").animate({
             scrollTop: e(this.hash).offset().top
         }, 1e3)
     })
 });
-
+$('#middleAnimated').css('top', $(document).height() / 2);
+$('#closetweet').click(function(){
+    $('#tweetbox').css('display', 'none');
+});
 
